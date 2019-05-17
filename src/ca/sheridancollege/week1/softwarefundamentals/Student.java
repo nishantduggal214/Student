0baddec61d0649dd0a285f3e118648c047a7d958
nishantduggal214 <duggalni@sheridancollege.ca>
2019-05-17 14:42:45 -0400

    2nd commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -10,8 +10,68 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * @author sidhu376
  */
 public class Student {
-    
+
     private String name;
+    private String id;
+
+    private String address;
+    
+    private String email;
+
+    /**
+     * Get the value of email
+     *
+     * @return the value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param email new value of email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    /**
+     * Get the value of address
+     *
+     * @return the value of address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Set the value of address
+     *
+     * @param address new value of address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return the value of id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param id new value of id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
 
     /**
      * Get the value of name
@@ -32,5 +92,3 @@ public class Student {
     }
 
 }
- 
-   
